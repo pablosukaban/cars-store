22c81f76dc671ab7c0926bbdf08e33164eff79e5
ilya <zapaska96@gmail.com>
2023-05-08 17:01:24 +0500

    роли

--- server/routes/carRouter.js
+++ server/routes/carRouter.js
@@ -1,9 +1,10 @@
 import { Router } from 'express';
 import carController from '../controllers/carController.js';
+import checkRoleMiddleware from '../middleware/checkRoleMiddleware.js';
 
 const router = new Router();
 
-router.post('/', carController.create);
+router.post('/', checkRoleMiddleware('ADMIN'), carController.create);
 router.get('/', carController.getAll);
 router.get('/:id', carController.getOne);
 
--- server/routes/modelRouter.js
+++ server/routes/modelRouter.js
@@ -1,9 +1,10 @@
 import { Router } from 'express';
 import modelController from '../controllers/modelController.js';
+import checkRoleMiddleware from '../middleware/checkRoleMiddleware.js';
 
 const router = new Router();
 
-router.post('/', modelController.create);
+router.post('/', checkRoleMiddleware('ADMIN'), modelController.create);
 router.get('/', modelController.getAll);
 
 export default router;
