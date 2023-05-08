86d47072171a68ac4323965c423e89eec5da4ea5
ilya <zapaska96@gmail.com>
2023-05-09 00:31:48 +0500

    херо

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,24 @@
+import { Suspense, lazy } from 'react';
 import Header from './components/Header';
+import { Routes, Route } from 'react-router-dom';
+import Loader from './components/Loader';
+
+const Home = lazy(() => import('./pages/Home'));
 
 const App = () => {
     return (
         <div>
             <Header />
+            <Routes>
+                <Route
+                    path='/'
+                    element={
+                        <Suspense fallback={<Loader />}>
+                            <Home />
+                        </Suspense>
+                    }
+                />
+            </Routes>
         </div>
     );
 };
---
+++ client/src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import Hero from '../components/Hero';
+
+const Home = () => {
+    return (
+        <main className='relative h-[200vh]'>
+            <Hero />
+            <div className='h-[1000px] bg-red-300'></div>
+        </main>
+    );
+};
+
+export default Home;
