ad6b5f1924cb9f2be2709d3190e5a534dec8d5ac
ilya <zapaska96@gmail.com>
2023-05-08 16:02:57 +0500

    регистрация

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,6 +1,46 @@
 import ApiError from '../error/ApiError.js';
+import Models from '../models/models.js';
+import bcrypt from 'bcrypt';
+import dotenv from 'dotenv';
+import jwt from 'jsonwebtoken';
+
+dotenv.config();
+
+const { User, Basket } = Models;
+
+const generateJwt = (id, email, role) => {
+    const newToken = jwt.sign({ id, email, role }, process.env.SECRET_KEY, {
+        expiresIn: '24h',
+    });
+
+    return newToken;
+};
+
 class UserController {
-    async registration(req, res) {}
+    async registration(req, res, next) {
+        const { email, password, role } = req.body;
+
+        if (!email || !password) {
+            return next(ApiError.badRequest('Неверный email или password'));
+        }
+
+        const candidate = await User.findOne({ where: { email } });
+
+        if (candidate) {
+            return next(
+                ApiError.badRequest('Пользователь с таким email уже существует')
+            );
+        }
+
+        const hashPassword = await bcrypt.hash(password, 5);
+
+        const user = await User.create({ email, password: hashPassword, role });
+
+        const basket = await Basket.create({ userId: user.id });
+
+        const token = generateJwt(user.id, user.email, user.role);
+        return res.json({ token });
+    }
     async login(req, res) {}
     async check(req, res, next) {
         const { id } = req.query;
