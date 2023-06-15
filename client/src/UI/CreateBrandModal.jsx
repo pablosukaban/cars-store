8c5cb2f2afc601929c80c9bf6eb3575a8a1e949c
ilya <zapaska96@gmail.com>
2023-06-15 11:30:36 +0500

    модалки для модели и бренда

--- client/src/UI/CreateBrandModal.jsx
+++ client/src/UI/CreateBrandModal.jsx
@@ -1,23 +1,28 @@
-import { useRef, useState } from 'react';
-import { createBrand, fetchBrands } from '../http/carAPI';
-import { carSlice } from '../store/carsSlice';
-import { useAppDispatch } from '../hooks/redux';
+import {useEffect, useRef, useState} from 'react';
+import {createBrand, fetchBrands} from '../http/carAPI';
+import {carSlice} from '../store/carsSlice';
+import {useAppDispatch} from '../hooks/redux';
 
-const CreateBrandModal = ({ isOpened, setIsOpened }) => {
+const CreateBrandModal = ({isOpened, setIsOpened}) => {
     const modalContainerRef = useRef(null);
+    const inputRef = useRef(null);
 
     const [value, setValue] = useState('');
+    const [loading, setLoading] = useState(false)
 
-    const { setBrands } = carSlice.actions;
+    const {setBrands} = carSlice.actions;
     const dispatch = useAppDispatch();
 
-    const handleSubmit = () => {
+    const handleSubmit = (e) => {
+        e.preventDefault()
+
         if (value.length === 0) {
             alert('Введие бренд');
             return;
         }
 
-        createBrand({ car_brand_name: value })
+        setLoading(true)
+        createBrand({car_brand_name: value})
             .then(() => {
                 setValue('');
                 setIsOpened(false);
@@ -32,6 +37,12 @@ const CreateBrandModal = ({ isOpened, setIsOpened }) => {
         }
     };
 
+    useEffect(() => {
+        if (!inputRef.current) return;
+
+        inputRef.current.focus()
+    }, [isOpened])
+
     return (
         <div
             className={`fixed left-0 top-0 z-[100] flex min-h-screen w-full items-center justify-center bg-gray-900 bg-opacity-50 ${
@@ -40,11 +51,13 @@ const CreateBrandModal = ({ isOpened, setIsOpened }) => {
             onClick={handleContainerClick}
             ref={modalContainerRef}
         >
-            <div className='flex flex-col items-center justify-center gap-6 rounded-lg bg-white p-6'>
+            <form onSubmit={handleSubmit}
+                  className='flex flex-col items-center justify-center gap-6 rounded-lg bg-white p-6'>
                 <h1 className='w-full border-b border-slate-300 p-1 text-2xl font-bold'>
                     Добавить бренд
                 </h1>
                 <input
+                    ref={inputRef}
                     placeholder='Название бренда'
                     value={value}
                     onChange={(e) => setValue(e.target.value)}
@@ -52,16 +65,15 @@ const CreateBrandModal = ({ isOpened, setIsOpened }) => {
                 />
                 <div className='flex w-full items-center justify-between gap-4 '>
                     <button
-                        onClick={handleSubmit}
-                        className='bg-primaryOrange px-4 py-3'
+                        className={`bg-primaryOrange px-4 py-3 ${loading ? 'opacity-50 cursor-wait' : ''}`}
                     >
                         Подтвердить
                     </button>
-                    <button onClick={() => setIsOpened(false)} className=''>
+                    <button type={'button'} onClick={() => setIsOpened(false)} className=''>
                         Отменить
                     </button>
                 </div>
-            </div>
+            </form>
         </div>
     );
 };
--- client/src/UI/CreateModelModal.jsx
+++ client/src/UI/CreateModelModal.jsx
@@ -1,29 +1,34 @@
-import { useRef, useState } from 'react';
-import { createModel, fetchModels } from '../http/carAPI';
-import { useAppDispatch } from '../hooks/redux';
-import { carSlice } from '../store/carsSlice';
+import {useEffect, useRef, useState} from 'react';
+import {createModel, fetchModels} from '../http/carAPI';
+import {useAppDispatch} from '../hooks/redux';
+import {carSlice} from '../store/carsSlice';
 
-const CreateModelModal = ({ isOpened, setIsOpened }) => {
+const CreateModelModal = ({isOpened, setIsOpened}) => {
     const modalContainerRef = useRef(null);
+    const inputRef = useRef(null);
 
     const [value, setValue] = useState('');
+    const [loading, setLoading] = useState(false)
 
-    const { setModels } = carSlice.actions;
+    const {setModels} = carSlice.actions;
     const dispatch = useAppDispatch();
 
-    const handleSubmit = () => {
+    const handleSubmit = (e) => {
+        e.preventDefault()
+
         if (value.length === 0) {
             alert('Введие модель');
             return;
         }
 
-        createModel({ car_model_name: value })
+        setLoading(true);
+        createModel({car_model_name: value})
             .then(() => {
                 setValue('');
                 setIsOpened(false);
             })
             .then(() => fetchModels())
-            .then((data) => dispatch(setModels(data)));
+            .then((data) => dispatch(setModels(data))).then(() => setLoading(false))
     };
 
     const handleContainerClick = (e) => {
@@ -31,6 +36,11 @@ const CreateModelModal = ({ isOpened, setIsOpened }) => {
             setIsOpened(false);
         }
     };
+
+    useEffect(() => {
+        if (!inputRef.current) return;
+        inputRef.current.focus()
+    }, [isOpened])
 
     return (
         <div
@@ -40,11 +50,13 @@ const CreateModelModal = ({ isOpened, setIsOpened }) => {
             onClick={handleContainerClick}
             ref={modalContainerRef}
         >
-            <div className='flex flex-col items-center justify-center gap-6 rounded-lg bg-white p-6'>
+            <form onSubmit={handleSubmit}
+                  className='flex flex-col items-center justify-center gap-6 rounded-lg bg-white p-6'>
                 <h1 className='w-full border-b border-slate-300 p-1 text-2xl font-bold'>
                     Добавить модель
                 </h1>
                 <input
+                    ref={inputRef}
                     placeholder='Название модели'
                     value={value}
                     onChange={(e) => setValue(e.target.value)}
@@ -52,16 +64,16 @@ const CreateModelModal = ({ isOpened, setIsOpened }) => {
                 />
                 <div className='flex w-full items-center justify-between gap-4 '>
                     <button
-                        onClick={handleSubmit}
-                        className='bg-primaryOrange px-4 py-3'
+                        className={`bg-primaryOrange px-4 py-3 ${loading ? 'opacity-50 cursor-wait' : ''}`}
+                        disabled={loading}
                     >
                         Подтвердить
                     </button>
-                    <button onClick={() => setIsOpened(false)} className=''>
+                    <button type={'button'} onClick={() => setIsOpened(false)} className=''>
                         Отменить
                     </button>
                 </div>
-            </div>
+            </form>
         </div>
     );
 };
