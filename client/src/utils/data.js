41914c1c6a68955014c2d57b8c5405055408b75a
ilya <zapaska96@gmail.com>
2023-06-15 21:42:33 +0500

    credit info

--- client/src/utils/data.js
+++ client/src/utils/data.js
@@ -1,42 +1,45 @@
 export const chooseUsArr = [
-    {
-        image: 'https://static.wixstatic.com/media/548a7f_dd830efd28744ea5bedf73697df6ae41.gif',
-        title: 'Сертифицированные автомобили',
-    },
-    {
-        image: 'https://static.wixstatic.com/media/548a7f_3cb8f7126a3e45a48be7deaa1ef6a2a8.gif',
-        title: 'Бесплатная пробная поездка',
-    },
-    {
-        image: 'https://static.wixstatic.com/media/548a7f_dece80c121f9407896aded8b1a82ba90.gif',
-        title: 'Бесплатная доставка',
-    },
-    {
-        image: 'https://static.wixstatic.com/media/548a7f_fbb07d3fdae947de80b4f07ab5ed96ff.gif',
-        title: 'Официальная гарантия',
-    },
-    {
-        image: 'https://static.wixstatic.com/media/548a7f_5429fc469c254739aee37fd14e708bed.gif',
-        title: 'Предварительное одобрение',
-    },
+  {
+    image: "https://static.wixstatic.com/media/548a7f_dd830efd28744ea5bedf73697df6ae41.gif",
+    title: "Сертифицированные автомобили",
+  },
+  {
+    image: "https://static.wixstatic.com/media/548a7f_3cb8f7126a3e45a48be7deaa1ef6a2a8.gif",
+    title: "Бесплатная пробная поездка",
+  },
+  {
+    image: "https://static.wixstatic.com/media/548a7f_dece80c121f9407896aded8b1a82ba90.gif",
+    title: "Бесплатная доставка",
+  },
+  {
+    image: "https://static.wixstatic.com/media/548a7f_fbb07d3fdae947de80b4f07ab5ed96ff.gif",
+    title: "Официальная гарантия",
+  },
+  {
+    image: "https://static.wixstatic.com/media/548a7f_5429fc469c254739aee37fd14e708bed.gif",
+    title: "Предварительное одобрение",
+  },
 ];
 
-const exampleText = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. ';
+const exampleText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. ";
 
 export const creditArr = [
-    {
-        image: 'https://static.wixstatic.com/media/548a7f_dd38d987ebf94ab4ad5e299ec6bf8712.png/v1/fill/w_56,h_65,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/548a7f_dd38d987ebf94ab4ad5e299ec6bf8712.png',
-        title: 'Быстро',
-        text: exampleText,
-    },
-    {
-        image: 'https://static.wixstatic.com/media/548a7f_a0848847eab54a9e830ddf6bde6615d5.png/v1/fill/w_70,h_58,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/548a7f_a0848847eab54a9e830ddf6bde6615d5.png',
-        title: 'Выгодно',
-        text: exampleText,
-    },
-    {
-        image: 'https://static.wixstatic.com/media/548a7f_bba67850940946b9927dd183eb598361.png/v1/fill/w_85,h_89,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/548a7f_bba67850940946b9927dd183eb598361.png',
-        title: 'Просто',
-        text: exampleText,
-    },
+  {
+    image:
+      "https://static.wixstatic.com/media/548a7f_dd38d987ebf94ab4ad5e299ec6bf8712.png/v1/fill/w_56,h_65,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/548a7f_dd38d987ebf94ab4ad5e299ec6bf8712.png",
+    title: "Быстро",
+    text: "Получите одобрение на автокредит всего за несколько часов и воплотите свою мечту о новом автомобиле без лишних задержек.",
+  },
+  {
+    image:
+      "https://static.wixstatic.com/media/548a7f_a0848847eab54a9e830ddf6bde6615d5.png/v1/fill/w_70,h_58,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/548a7f_a0848847eab54a9e830ddf6bde6615d5.png",
+    title: "Выгодно",
+    text: "Наслаждайтесь низкими процентными ставками и гибкими условиями погашения кредита, чтобы сэкономить деньги и получить максимальную выгоду.",
+  },
+  {
+    image:
+      "https://static.wixstatic.com/media/548a7f_bba67850940946b9927dd183eb598361.png/v1/fill/w_85,h_89,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/548a7f_bba67850940946b9927dd183eb598361.png",
+    title: "Просто",
+    text: "Мы сделали процесс получения кредита максимально простым и удобным, чтобы вы могли с легкостью осуществить свою мечту.",
+  },
 ];
