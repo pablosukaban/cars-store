1ebef64b21bf85026d7c40b7fe97939554562191
ilya <zapaska96@gmail.com>
2023-06-16 16:54:57 +0500

    controller tabs

--- server/controllers/carController.js
+++ server/controllers/carController.js
@@ -1,121 +1,121 @@
-import {v4 as uuidv4} from 'uuid';
-import path from 'path';
+import { v4 as uuidv4 } from "uuid";
+import path from "path";
 
-import Models from '../models/models.js';
+import Models from "../models/models.js";
 
-import ApiError from '../error/ApiError.js';
+import ApiError from "../error/ApiError.js";
 
-const {Car, CarInfo} = Models;
+const { Car, CarInfo } = Models;
 
 class CarController {
-    async create(req, res, next) {
-        try {
-            let {
-                car_model_id,
-                car_brand_id,
-                year_of_manufacture,
-                body_type,
-                car_color,
-                car_price,
-                info,
-                car_description
-            } = req.body;
-            const {car_image} = req.files;
+  async create(req, res, next) {
+    try {
+      let {
+        car_model_id,
+        car_brand_id,
+        year_of_manufacture,
+        body_type,
+        car_color,
+        car_price,
+        info,
+        car_description
+      } = req.body;
+      const { car_image } = req.files;
 
-            const fileName = uuidv4() + '.jpg';
+      const fileName = uuidv4() + ".jpg";
 
-            car_image.mv(path.resolve('static', fileName));
+      car_image.mv(path.resolve("static", fileName));
 
-            const car = await Car.create({
-                carModelId: car_model_id,
-                carBrandId: car_brand_id,
-                year_of_manufacture,
-                body_type,
-                car_color,
-                car_price,
-                car_image: fileName,
-                car_description
-            });
+      const car = await Car.create({
+        carModelId: car_model_id,
+        carBrandId: car_brand_id,
+        year_of_manufacture,
+        body_type,
+        car_color,
+        car_price,
+        car_image: fileName,
+        car_description
+      });
 
-            if (info) {
-                info = JSON.parse(info);
-                info.forEach((element) => {
-                    CarInfo.create({
-                        title: element.title,
-                        description: element.description,
-                        carId: car.id,
-                    });
-                });
-            }
-
-            return res.json(car);
-        } catch (e) {
-            next(ApiError.badRequest(e.message));
-        }
-    }
-
-    async getAll(req, res) {
-        let {carModelId, carBrandId, limit, page} = req.query;
-
-        page = page || 1;
-        limit = limit || 9;
-        let offset = page * limit - limit;
-
-        let cars;
-
-        if (!carBrandId && !carModelId) {
-            cars = await Car.findAndCountAll({limit, offset});
-        }
-
-        if (carBrandId && !carModelId) {
-            cars = await Car.findAndCountAll({
-                where: {carBrandId},
-                limit,
-                offset,
-            });
-        }
-
-        if (!carBrandId && carModelId) {
-            cars = await Car.findAndCountAll({
-                where: {carModelId},
-                limit,
-                offset,
-            });
-        }
-
-        if (carBrandId && carModelId) {
-            cars = await Car.findAndCountAll({
-                where: {carModelId, carBrandId},
-                limit,
-                offset,
-            });
-        }
-
-        return res.json(cars);
-    }
-
-    async getOne(req, res) {
-        const {id} = req.params;
-
-        const car = await Car.findOne({
-            where: {id},
-            include: [{model: CarInfo, as: 'info'}],
+      if (info) {
+        info = JSON.parse(info);
+        info.forEach((element) => {
+          CarInfo.create({
+            title: element.title,
+            description: element.description,
+            carId: car.id
+          });
         });
+      }
 
-        return res.json(car);
+      return res.json(car);
+    } catch (e) {
+      next(ApiError.badRequest(e.message));
+    }
+  }
+
+  async getAll(req, res) {
+    let { carModelId, carBrandId, limit, page } = req.query;
+
+    page = page || 1;
+    limit = limit || 9;
+    let offset = page * limit - limit;
+
+    let cars;
+
+    if (!carBrandId && !carModelId) {
+      cars = await Car.findAndCountAll({ limit, offset });
     }
 
-    async delete(req, res) {
-        const {id} = req.params;
-
-        await Car.destroy({
-            where: {id},
-        });
-
-        return res.json({
-            message: 'Car deleted',
-        })
+    if (carBrandId && !carModelId) {
+      cars = await Car.findAndCountAll({
+        where: { carBrandId },
+        limit,
+        offset
+      });
     }
+
+    if (!carBrandId && carModelId) {
+      cars = await Car.findAndCountAll({
+        where: { carModelId },
+        limit,
+        offset
+      });
+    }
+
+    if (carBrandId && carModelId) {
+      cars = await Car.findAndCountAll({
+        where: { carModelId, carBrandId },
+        limit,
+        offset
+      });
+    }
+
+    return res.json(cars);
+  }
+
+  async getOne(req, res) {
+    const { id } = req.params;
+
+    const car = await Car.findOne({
+      where: { id },
+      include: [{ model: CarInfo, as: "info" }]
+    });
+
+    return res.json(car);
+  }
+
+  async delete(req, res) {
+    const { id } = req.params;
+
+    await Car.destroy({
+      where: { id }
+    });
+
+    return res.json({
+      message: "Car deleted"
+    });
+  }
 }
 
 export default new CarController();
